b717021e9f10524bd7909e206037359d56e77121
Adilet Jyrgalbekov <you@example.com>
2024-06-09 23:21:13 +0600

    first commit

---
+++ Java-main/labs/lab3/Library.java
@@ -0,0 +1,61 @@
+package lab3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("добавленная книга: " + book);
+    }
+
+    public void removeBook(String isbn) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getIsbn().equals(isbn)) {
+                iterator.remove();
+                System.out.println("удаленная книга: " + book);
+                return;
+            }
+        }
+        System.out.println("книга с таким ISBN " + isbn + " не найдена.");
+    }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("в библиотеке нет книг");
+        } else {
+            System.out.println("книги в библиотеке:");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234567890");
+        Book book2 = new Book("1984", "George Orwell", "1234567891");
+        Book book3 = new Book("To Kill a Mockingbird", "Harper Lee", "1234567892");
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        library.displayBooks();
+
+        library.removeBook("1234567891");
+
+        library.displayBooks();
+    }
+}
+
